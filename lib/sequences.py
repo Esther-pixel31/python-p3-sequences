af4350cec8e0bf7bb469c0954912111b937943d4
EstherMutua <esthermutua16@gmail.com>
2025-05-07 20:44:28 +0300

    hate tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])  # No elements requested
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    sequence = [0, 1]
+    while len(sequence) < length:
+        next_value = sequence[-1] + sequence[-2]
+        sequence.append(next_value)
+    
+    print(sequence)
